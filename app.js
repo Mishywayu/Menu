7df51705bb4ef8122001b7653e0333130ef47a48
Mishywayu <michellewayua2@gmail.com>
2022-10-27 22:51:38 +0300

    My first Node.JS project

---
+++ app.js
@@ -0,0 +1,35 @@
+//acquiring our express module
+const express = require('express');
+
+const app = express();
+
+//set templating engine as Ejs
+app.set('view engine','ejs');
+
+//servin static file
+app.use(express.static('public'));
+
+//Routing
+//Route for Home (/)
+app.get('/', (req, res) => {
+    res.render('Home');
+});
+//route for about us
+app.get('/about', (req, res) => {
+    res.render('About');
+});
+//route for menu
+app.get('/menu', (req, res) => {
+    res.render('Menu');
+}
+);
+//route for contact us
+app.get('/contact', (req, res) => {
+    res.render('Contact');
+});
+
+
+//creating a server
+app.listen(3000, () => {
+    console.log('Server is running');
+});
