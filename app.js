7d8a67d371ad4c4f4ab47341ba840d85b5e06ccb
Mishywayu <michellewayua2@gmail.com>
2022-11-09 20:29:34 +0300

    node.js

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 //acquiring our express module
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
 //set templating engine as Ejs
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-//servin static file
+//serving static file
 app.use(express.static('public'));
+//body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+//database url
+const url = 'mongodb+srv://mishykitch:123456789@projectexpress.7gxtzyu.mongodb.net/?retryWrites=true&w=majority';
+//connecting application with DB
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log("Mongo DB connected!")).catch(err => console.log(err));
+
+//import Book model
+const Booktable = require('./models/Booktable');
 
 //Routing
 //Route for Home (/)
@@ -27,12 +45,26 @@ app.get('/menu', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('Contact');
 });
-//route for order
-app.get('/order', (req, res) => {
-    res.render('Order');
+//route for book
+app.get('/book', (req, res) => {
+    res.render('Book');
 });
+//ROUTE FOR SAVING BOOK TO DB
+app.post('/book-table', (req, res) => {
+    //save the data to the DB
+    const Data = new Booktable({
+        fullname: req.body.fullname,
+        email: req.body.email,
+        phone: req.body.phone,
+        meal: req.body.meal,
+        persons: req.body.persons,
+        date: req.body.date
+    });
 
-
+    Data.save().then(() => {
+        res.redirect('/');
+    }).catch(err => console.log(err));
+});
 //creating a server
 app.listen(3000, () => {
     console.log('Server is running');
